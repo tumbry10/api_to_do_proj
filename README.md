7ac2152aa18b78f0a2e5d9948b51886dddebe6ae
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2025-06-13 11:47:09 +0200

    Backend Development for the CRUD Operation

---
+++ README.md
@@ -0,0 +1,5 @@
+TO DO List API Project
+
+=====================================
+This Project is a simple to-do list API that allows users to create, read, update, and delete tasks
+The API is built using Django Rest Framework and uses a SQLite database for storage
--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False, blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
